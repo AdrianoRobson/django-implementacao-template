74926c3a030ce1dbaed620374ce8a2f38a8271ae
adriano <adriano08andrade@hotmail.com>
2021-11-18 07:27:52 -0300

    Adiçao de classes responsaveis pelo conteudo das sections do index.html

--- core/models.py
+++ core/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from stdimage.models import StdImageField
 
-# Create your models here.
+class Base(models.Model):
+    criado = models.DateField('criação', auto_now_add=True)
+    modificado = models.DateTimeField('Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo', default=True)
+
+    class Meta:
+        abstract = True
+
+class Servico(Base):
+
+    CHOISE_ICON = (
+        ('lni-cog', 'Engrenagem'),
+        ('lni-stats-up', 'Gráfico'),
+        ('lni-users', 'Usuários'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Foguete'),
+    )      
+
+    servico = models.CharField('Nome', max_length=100)
+    descricao = models.TextField('Descricao', max_length=200)
+    icone = models.CharField('Icone', max_length=16, choices=CHOISE_ICON)
+    
+    class Meta:
+        verbose_name = "Serviço"
+        verbose_name_plural = "Serviços"
+    
+    def __str__(self):
+        return self.servico
+
+class Cargo(Base):
+    cargo = models.CharField('Cargo', max_length=100)
+
+    class Meta:
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+
+    def __str__(self):
+        return self.cargo
+
+
+class Funcionario(Base):
+    nome = models.CharField('Nome', max_length=100)
+    cargo = models.ForeignKey('core.Cargo', verbose_name='Cargo', on_delete=models.CASCADE)
+    bio = models.TextField('Descrição', max_length=200)
+    imagem = StdImageField('imagem', upload_to='equipe', variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
+    facebook = models.CharField('facebook', max_length=100, default='#')
+    twitter = models.CharField('twitter', max_length=100, default='#')
+    instagram = models.CharField('instagram', max_length=100, default='#')
+
+    class Meta:
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
+
+    def __str__(self):
+        return self.nome
+    
