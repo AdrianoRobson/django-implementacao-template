f1f67d1c69d183f35c989fe0ce3a2c15bd1c70d0
adriano <adriano08andrade@hotmail.com>
2021-11-18 09:52:22 -0300

    Adiçao do uuid(para renomear arquivos) no campo imagem do model funcionario.

--- core/models.py
+++ core/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from stdimage.models import StdImageField
+import uuid
+
+def get_file_path(_instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return filename
+
 
 class Base(models.Model):
     criado = models.DateField('criação', auto_now_add=True)
@@ -46,7 +53,7 @@ class Funcionario(Base):
     nome = models.CharField('Nome', max_length=100)
     cargo = models.ForeignKey('core.Cargo', verbose_name='Cargo', on_delete=models.CASCADE)
     bio = models.TextField('Descrição', max_length=200)
-    imagem = StdImageField('imagem', upload_to='equipe', variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
+    imagem = StdImageField('imagem', upload_to=get_file_path, variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
     facebook = models.CharField('facebook', max_length=100, default='#')
     twitter = models.CharField('twitter', max_length=100, default='#')
     instagram = models.CharField('instagram', max_length=100, default='#')
