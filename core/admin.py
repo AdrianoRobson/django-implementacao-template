b282e1c35e3aac3e2b5dd70d41cb388306e7865c
adriano <adriano08andrade@hotmail.com>
2021-11-18 08:09:36 -0300

    Registro de alguns  models no admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Cargo, Funcionario, Servico
 
-# Register your models here.
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico', 'descricao', 'icone', 'ativo','modificado')
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'modificado')
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'bio', 'imagem', 'ativo', 'modificado')
