67d894b98ff8a7d1485bbe177fc622b114cfcbae
adriano <adriano08andrade@hotmail.com>
2021-11-19 15:48:35 -0300

    Conclusão de testes do models.py

--- core/testes/test_models.py
+++ core/testes/test_models.py
@@ -11,3 +11,28 @@ class GetFilePathTestCase(TestCase):
     def test_get_file_path(self):
         arquivo = get_file_path(None, 'teste.png')
         self.assertTrue(len(arquivo), len(self.filename))
+
+class ServicoTestCase(TestCase):
+
+    def setUp(self):
+        self.servico = mommy.make('Servico')
+    
+    def test_str(self):
+        self.assertEquals(str(self.servico), self.servico.servico)
+
+class CargoTestCase(TestCase):
+
+    def setUp(self):
+        self.cargo = mommy.make('Cargo')
+
+    def test_str(self):
+        self.assertEquals(str(self.cargo), self.cargo.cargo)
+
+class FuncionarioTestCase(TestCase):
+
+    def setUp(self):
+        self.funcioanario = mommy.make('Funcionario')
+
+    def test_str(self):
+        self.assertAlmostEquals(str(self.funcioanario), self.funcioanario.nome)
+
