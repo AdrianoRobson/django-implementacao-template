f202a7318406d5616e2a7883c647c3cf8bea4bac
adriano <adriano08andrade@hotmail.com>
2021-11-18 21:44:48 -0300

    Correção de acesso ao diretório media/ para /media/

--- template_django/settings.py
+++ template_django/settings.py
@@ -123,6 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = path.join(BASE_DIR, 'media')
