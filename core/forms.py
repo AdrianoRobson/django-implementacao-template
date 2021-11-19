a987e0ed883ee81f349289e05764aa923599499f
adriano <adriano08andrade@hotmail.com>
2021-11-19 07:16:58 -0300

    Criação e definição do arquivo forms.py para envio de email

---
+++ core/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.core.email.message import EmailMessage
+
+class ContatoForm(forms.Form):
+    nome = forms.CharField(label='Nome', max_length=100)
+    email = forms.EmailField(label='Email', max_length=100)
+    assunto = forms.CharField(label='Assunto', max_length=100)
+    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea)
+
+    def send_mail(self):
+        nome = self.cleaned_data['nome']
+        email = self.cleaned_data['email']
+        assunto = self.cleaned_data['assunto']
+        mensagem = self.cleaned_data['mensagem']
+
+        content = f'Nome: {nome}\n'\
+            f'E-mail: {email}\n'\
+            f'Assunto: {assunto}\n'\
+            f'Mensagem: {mensagem}'
+        
+        mail =EmailMessage(
+            subject=assunto,
+            body=content,
+            from_email='teste@template.com.br',
+            to=['teste2@template.com.br'],
+            headers={'Replay-To: ': email}
+        )
+
+        mail.send()
